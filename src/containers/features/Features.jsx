a89ddf4d788dfdaf6dcfe6051aae418347f0ee74
GCremez <salauolorunjuedalo@gmail.com>
2023-08-01 16:49:13 +0300

    Features

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -6,7 +6,7 @@ const Features = () => {
     <div className='gpt__features section__padding' id='features'>
       <div className="gpt__features-heading">
         <h1 className="gradient__text">The Future is Now and You Just Need To Realise It. Step into Future Today & Make it Happen</h1>
-        <p></p>
+        <p>Request Early Access to Get Started</p>
       </div>
     </div>
   )
